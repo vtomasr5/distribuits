d12529f50723199448fe58a598f70dadaff8bbea
Miquel B <m@m.com>
2013-03-07 22:34:20 +0100

    Obtención de las primeras 5 páginas por parámetro

--- noticias.py
+++ noticias.py
@@ -2,10 +2,14 @@
 from meneame.obtenerNoticias import ObtenerNoticias
 
 o = ObtenerNoticias()
-res = o.get()
-#falta pasar de unicode a utf-8
-for index,item in enumerate(res):
-    print "------------------------------------------------------------------"
-    print "#"+ str(index)
-    print item
-    print "------------------------------------------------------------------" 
+for i in range(1,5):
+    res = o.get(i)
+    #falta pasar de unicode a utf-8
+    print "------------------------------------------------------------------------------------------------------------->"+ str(i)
+    for index,item in enumerate(res):
+        print "------------------------------------------------------------------"
+        print "#"+ str(index)
+        print item
+        print "------------------------------------------------------------------"
+        
+    print "<-------------------------------------------------------------------------------------------------------------" 
