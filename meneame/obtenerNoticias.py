3160320e0753bd7d0402a3b4224e47853809315d
Vicenç Juan Tomàs Montserrat <vtomasr5@gmail.com>
2013-03-09 12:39:15 +0100

    add comments. not working 100% now :(

--- meneame/obtenerNoticias.py
+++ meneame/obtenerNoticias.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
-from urllib2 import urlopen, URLError, HTTPError
-from urlparse import urlparse, urlsplit, urljoin
-import re, os, sys
+from urllib2 import urlopen, URLError
+import re
+import httplib
 from properties import MENEAME_URL
 
-class ObtenerNoticias:
+class ObtenerNoticias(object):
     
     def __init__(self):
         self._html = None
@@ -15,18 +15,32 @@ class ObtenerNoticias:
             return doc.read()
         except:
             return None    
+
+    def _obtener_contenido_url(self, url):
+        try:
+            html = urlopen(url).read()
+            return html
+        except:
+            return None
+
+    def _existeix_url(self, url):
+        try:
+            code = urlopen(url).code
+            if code == 200:
+                return True
+        except:
+            return False    
     
     def _obtener_items(self, regexpr1, regexpr2):
-        list = []
+        l = []
         g = re.findall(regexpr1, self._html, re.I|re.S)
         if g:
             for m in g:
                 p = re.search(regexpr2, m, re.I)
                 if p:
-                    l = p.groups()[0].strip()
-                    list.append(l)
-        
-        return list
+                    n = p.groups()[0].strip()
+                    l.append(n)
+        return l
     
     def _obtener_links(self):
         regexpr1 = r'<h2> <a href=".*?" class'
@@ -57,8 +71,43 @@ class ObtenerNoticias:
          
         return self._obtener_items(regexpr1,regexpr2)
 
-    def _make_noticias(self, contenido ):
-        list = []
+    def _obtener_links_noticias(self, html):
+        links = []
+        while True:
+            start = html.find('class="comments-counter"')
+            if start == -1:
+                break
+            start_link = html.find('<a href="', start)
+            end_link = html.find('"', start_link+9)
+            link = html[start_link+9:end_link]
+            menurl = MENEAME_URL[7:]
+            links.append(menurl+link+'/')
+            html = html[start+1:]
+        return links
+
+    def _obtener_comentarios(self):
+        comentaris = []
+        links = self._obtener_links_noticias(self._html)
+        for link in links:
+            for i in range(1,5):
+                if self._existeix_url(link+str(i)):
+                    html_noticia = self._obtener_contenido_url(link)
+                    while True:
+                        start_link = html_noticia.find('id="cid-')
+                        if start_link == -1:
+                            break
+                        end_link = html_noticia.find('</div>', start_link+1)
+                        html_comentari = html_noticia[start_link:end_link]
+
+                        start_link2 = html_comentari.find('</a>')
+                        end_link2 = html_comentari.find('</div>', start_link2+1)
+                        
+                        comentaris.append(html_comentari[start_link2+16:end_link2])
+                        html_noticia = html_noticia[end_link+1:]
+        return comentaris
+
+    def _make_noticias(self, contenido):
+        l = []
         min=0
         max = len(contenido['descripciones'])
         
@@ -67,14 +116,16 @@ class ObtenerNoticias:
             contenido['descripciones'].pop(0)
         
         for i in range(min,max):
-            list.append({ 'titulo': contenido['titulos'][i],
+            l.append({ 'titulo': contenido['titulos'][i],
                  'link': contenido['links'][i],
                  'meneos': contenido['meneos'][i],
                  'descripcion': contenido['descripciones'][i],
                  'autor': contenido['autores'][i],
+                 'link_noticia': contenido['links_noticias'][i],
+                 'comentario': contenido['comentarios'][i]
                 })
         
-        return list
+        return l
 
     def get(self, pagina=1):
         self._html = self._obtener_contenido(pagina)
@@ -84,5 +135,7 @@ class ObtenerNoticias:
         contenido['meneos'] = self._obtener_meneos()
         contenido['descripciones'] = self._obtener_descripciones()
         contenido['autores'] = self._obtener_autores()
+        contenido['links_noticias'] = self._obtener_links_noticias(self._html)
+        contenido['comentarios'] = self._obtener_comentarios()
         
         return self._make_noticias(contenido)
--- noticias.py
+++ noticias.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 from meneame.obtenerNoticias import ObtenerNoticias
+from meneame.properties import PAGINA
 
-PAGINA = 1
-
-o = ObtenerNoticias()
-res = o.get(PAGINA)
-#falta pasar de unicode a utf-8
-for index,item in enumerate(res):
-    print "------------------------------------------------------------------"
-    print "#"+ str(index)
-    print item
-    print "------------------------------------------------------------------" 
+if __name__ == '__main__':
+    o = ObtenerNoticias()
+    res = o.get(PAGINA)
+    #falta pasar de unicode a utf-8
+    for index,item in enumerate(res):
+        print "------------------------------------------------------------------"
+        print "#"+ str(index)
+        print item
+        print "------------------------------------------------------------------" 
