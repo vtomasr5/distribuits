ed41b687d60be1c13c1c60f0f4f92aabf72d8c69
Miquel B <m@m.com>
2013-03-28 13:51:01 +0100

    Corregido noticias duplicadas

--- noticiasSql.py
+++ noticiasSql.py
@@ -20,6 +20,8 @@ status = sys.argv[3]
 
 url_news = ''
 
+all_news=[]
+
 if not status in ('published', 'pending'):
 	print 'python noticiasSql.py <pag_inicial> <pag_final> <published|pending>'
 	sys.exit()
@@ -42,52 +44,54 @@ for i in reversed(range(init-1,max)):
 	if status== 'queued':
 		res.reverse()
 	for index,item in (enumerate(res)):
-		b = b + "insert into users (user_id,user_login,user_pass) values (NULL,'"+ item['autor'] +"', '0000');\n\n"
-		b = b + "insert into links (link_id,link_content,link_title,link_url,link_votes,link_author,"
-		b = b + "link_tags,link_status,link_sent_date"
-		if status == 'published':
-			b = b + ",link_date"
-		#b = b + ") values ("+str(i)+", '" 
-		b = b + ") values (NULL, '" 
-		b = b + limpia(item['descripcion']) + "',"
-		b = b + "'" + limpia(item['titulo']) + "',"
-		b = b + "'" + item['link'] + "',"
-		b = b + item['meneos'] + ","
-		b = b + "(Select user_id from users where user_login ='"+ item['autor'] +"'),"
-		#falten es tags, es pos un xk sinos no va be
-		b = b + "'" + limpia(item['tags'][0]) + "-" +  limpia(item['tags'][1]) + "',"
-		#print str(item['fechaEnvio'])
-		k = re.split(".",str(item['fechaEnvio']))
-		#print k
-		k1 = re.split(".",str(item['fechaPublicacion']))
-		b = b + "'" + status +"','"+str(item['fechaEnvio'])
-		if status == 'published':
-			b = b +"','"+str(item['fechaPublicacion'])
-		b = b + "');" "\n\n"
-		f.write(b)
-		f1.write(b)
-		comment_id = 0
-		b ="SET @id_link = (select max(link_id) from links);\n\n"
-		f.write(b)
-		f1.write(b)
-		for comment in (enumerate(item['comentario'])):
-			if comment_id < len(item['fecha_comentario']):#Esto está asi para que no pete
-				comentario_fecha = item['fecha_comentario'][comment_id]
-			if comment_id < len(item['autor_comentario']):
-				comentario_autor = item['autor_comentario'][comment_id]
-			c = c +"insert into users (user_id,user_login,user_pass) values (NULL,'"+ comentario_autor +"', '0000');\n\n"
-			c = c + "insert into comments (comment_id,comment_type,comment_link_id,comment_content,"
-			c = c + "comment_user_id,comment_date)values (NULL,'normal',@id_link,'"+limpia1(str(comment))+"',"
-			c = c + "(Select user_id from users where user_login ='"+ comentario_autor +"'),'"+str(comentario_fecha)+"');\n\n"
-			c = c + "SET @nombre_var = @nombre_var + 1; \n\n"
-			f.write(c)
-			f1.write(c)
-			c =""
-			comment_id = comment_id + 1
-		b = "update links set link_comments="+str(len(item['comentario']))+" where link_id=@id_link;"+"\n\n"
-		f.write(b)
-		f1.write(b)
-		b=""
-		i = i + 1
+		if not item['titulo'] in all_news:
+			all_news.append(item['titulo'])
+			b = b + "insert into users (user_id,user_login,user_pass) values (NULL,'"+ item['autor'] +"', '0000');\n\n"
+			b = b + "insert into links (link_id,link_content,link_title,link_url,link_votes,link_author,"
+			b = b + "link_tags,link_status,link_sent_date"
+			if status == 'published':
+				b = b + ",link_date"
+			#b = b + ") values ("+str(i)+", '" 
+			b = b + ") values (NULL, '" 
+			b = b + limpia(item['descripcion']) + "',"
+			b = b + "'" + limpia(item['titulo']) + "',"
+			b = b + "'" + item['link'] + "',"
+			b = b + item['meneos'] + ","
+			b = b + "(Select user_id from users where user_login ='"+ item['autor'] +"'),"
+			#falten es tags, es pos un xk sinos no va be
+			b = b + "'" + limpia(item['tags'][0]) + "-" +  limpia(item['tags'][1]) + "',"
+			#print str(item['fechaEnvio'])
+			k = re.split(".",str(item['fechaEnvio']))
+			#print k
+			k1 = re.split(".",str(item['fechaPublicacion']))
+			b = b + "'" + status +"','"+str(item['fechaEnvio'])
+			if status == 'published':
+				b = b +"','"+str(item['fechaPublicacion'])
+			b = b + "');" "\n\n"
+			f.write(b)
+			f1.write(b)
+			comment_id = 0
+			b ="SET @id_link = (select max(link_id) from links);\n\n"
+			f.write(b)
+			f1.write(b)
+			for comment in (enumerate(item['comentario'])):
+				if comment_id < len(item['fecha_comentario']):#Esto está asi para que no pete
+					comentario_fecha = item['fecha_comentario'][comment_id]
+				if comment_id < len(item['autor_comentario']):
+					comentario_autor = item['autor_comentario'][comment_id]
+				c = c +"insert into users (user_id,user_login,user_pass) values (NULL,'"+ comentario_autor +"', '0000');\n\n"
+				c = c + "insert into comments (comment_id,comment_type,comment_link_id,comment_content,"
+				c = c + "comment_user_id,comment_date)values (NULL,'normal',@id_link,'"+limpia1(str(comment))+"',"
+				c = c + "(Select user_id from users where user_login ='"+ comentario_autor +"'),'"+str(comentario_fecha)+"');\n\n"
+				c = c + "SET @nombre_var = @nombre_var + 1; \n\n"
+				f.write(c)
+				f1.write(c)
+				c =""
+				comment_id = comment_id + 1
+			b = "update links set link_comments="+str(len(item['comentario']))+" where link_id=@id_link;"+"\n\n"
+			f.write(b)
+			f1.write(b)
+			b=""
+			i = i + 1
 	f.close()
 f1.close()
