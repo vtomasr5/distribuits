3e46213ebc6700e74487ac8f0d63ce0fed553b8b
Miquel B <m@m.com>
2013-03-31 16:38:27 +0200

    Añadido try catch para impedir que se utilicen operaciones no permitidas

--- simulacion/client.py
+++ simulacion/client.py
@@ -25,10 +25,15 @@ class Client(threading.Thread):
 
 	def run(self):
 		while not self.exitFlag:
-			msg = self._mailbox.get()
-			operation = msg['operation']
-			param = msg['parameter']
-			self.operation[operation](param)
+			try:
+				msg = self._mailbox.get()
+				operation = msg['operation']
+				param = msg['parameter']
+				self.operation[operation](param)
+			except KeyError: #Operation not in self.operation
+				pass
+			except KeyboardInterrupt: #CTRL+C interrupt
+				return
 
 	def _open_connection(self):
 		self._connection = httplib.HTTPConnection(self.url)
