878f0b7afd2947ab9220162cbe72514928035d39
Miquel B <mike@ubuntu.(none)>
2013-04-11 16:39:38 +0200

    Modificat stats.py

--- stats.py
+++ stats.py
@@ -6,18 +6,32 @@ from meneame.properties import MENEAME_COMENTARIOS_RSS
 from meneame.properties import MENEAME_PENDIENTES_RSS
 from meneame.properties import DIROUT
 import os
+import datetime
+import time
 
-# check if exists output dir
-path = get_path()
-d = os.listdir(path)
-if DIROUT not in d:
-    os.mkdir(DIROUT)
+#FIXED PARAMETERS
+MINUTES = 10		#CADA CUANTO SE VA A EJECUTAR
+NUMBER_OF_DAYS = 1  #CUANTOS DIAS SE VA A EJECUTAR
 
-stats_base 		  = obtenerEstadisticas(path, MENEAME_BASE_RSS)
-stats_comentarios = obtenerEstadisticas(path, MENEAME_COMENTARIOS_RSS)
-stats_pendientes  = obtenerEstadisticas(path, MENEAME_PENDIENTES_RSS)
+SLEEP = 60*MINUTES
 
-stats_base.getStats()
-stats_comentarios.getStats()
-stats_pendientes.getStats()
+end = datetime.datetime.now() + datetime.timedelta(days=NUMBER_OF_DAYS)
+
+
+while datetime.datetime.now() < end:
+	# check if exists output dir
+	path = get_path()
+	d = os.listdir(path)
+	if DIROUT not in d:
+	    os.mkdir(DIROUT)
+
+	stats_base 		  = obtenerEstadisticas(path, MENEAME_BASE_RSS)
+	stats_comentarios = obtenerEstadisticas(path, MENEAME_COMENTARIOS_RSS)
+	stats_pendientes  = obtenerEstadisticas(path, MENEAME_PENDIENTES_RSS)
+
+	stats_base.getStats()
+	stats_comentarios.getStats()
+	stats_pendientes.getStats()
+	print "Durmiendo..."
+	time.sleep(SLEEP) #Sleep 5 minutos
 
