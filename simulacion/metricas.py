d1eb7dc140e574320cefcfe6d65bfcc7f9f4fda5
Vicenç Juan Tomàs Montserrat <vtomasr5@gmail.com>
2013-04-25 23:15:20 +0200

    prova metriques

--- simulacion/metricas.py
+++ simulacion/metricas.py
@@ -2,14 +2,44 @@
 
 import psutil as ps
 import threading
+import time
 
 
-def mesura():
-    print str(ps.cpu_percent()) + "%"
+def mesura_cpu():
+    print "CPU usage: " + str(ps.cpu_percent()) + "%"
+
+
+def mesura_mem():
+    print "Mem usage: " + str(ps.virtual_memory()[2]) + "%"
+
+
+def mesura_net():
+    print "Bytes sent: " + str(ps.network_io_counters()[0])
+    print "Bytes received: " + str(ps.network_io_counters()[1])
+
+
+def mesura_disk():
+    print "Read bytes: " + str(ps.disk_io_counters()[2])
+    print "Write bytes: " + str(ps.disk_io_counters()[3])
+
+
+def handler(mesura):
+    if mesura == 'cpu':
+        mesura_cpu()
+    elif mesura == 'mem':
+        mesura_mem()
+    elif mesura == 'net':
+        mesura_net()
+    elif mesura == 'disk':
+        mesura_disk()
+    else:
+        raise NameError("Parametre incorrecte")
 
 # Main
 threads = []
-for i in range(5):
-    t = threading.Thread(target=mesura)
+m = ('cpu', 'mem', 'net', 'disk')
+for i in m:
+    t = threading.Thread(target=handler, args=(i,))  # NO llevar sa coma de i,
     threads.append(t)
+    time.sleep(0.02)
     t.start()
