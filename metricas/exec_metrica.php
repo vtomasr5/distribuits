67801baf1fd5a08ca766ed7e0a214cd403a7e8a4
Miquel <mike@Saturno>
2013-04-26 18:05:35 +0200

    Añadido clave en request get al exec_metricas.php

--- metricas/exec_metrica.php
+++ metricas/exec_metrica.php
@@ -1,9 +1,18 @@
 <?php
-
+/**
+*
+*	Script PHP que ejecutara el script python con una llamada desde el navegador
+*	exec_metrica.php?pw=CLAVE
+*/
+define("CLAVE", "clave_ejecucion");
 $ruta	= '/home/meneame/distribuits/metricas/'
 $script = 'metrica.py'
 
-exec("python ".$ruta.$script, $output);
-echo '<pre>';
-print_r($output);
-echo '</pre>';
+if (isset($_GET['pw'])) {
+	if ($_GET['pw'] == CLAVE) {
+		exec("python ".$ruta.$script, $output);
+		echo '<pre>';
+		print_r($output);
+		echo '</pre>';
+	}
+}
