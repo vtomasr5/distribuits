bb9d1dc12e34593c627f60ce5ecd2ac8f53a945b
Miquel <mike@Saturno>
2013-04-28 15:21:51 +0200

    Corregido Script exec_metrica.php para ejecutarse en background

--- metricas/exec_metrica.php
+++ metricas/exec_metrica.php
@@ -7,17 +7,19 @@
 */
 define("CLAVE", "clave_ejecucion");
 $ruta	= '/home/meneame/distribuits/metricas/';
-$script = 'metrica.py';
+$script = 'metricav2.py';
+
+//Run linux command in background and return the PID created by the OS
 
 if (isset($_GET['pw'])) {
 	if ($_GET['pw'] == CLAVE) {
-		$instruccion = ''
+		$instruccion = "cd ".$ruta." && ";
 		if (!isset($_GET['stop'])) {
-			$instruccion = "python ".$ruta.$script;
+			$instruccion .= "/usr/bin/python ".$ruta.$script;
 		} else {
 			$instruccion = "echo '' > ".$ruta.'exec.stop';			
 		}
-		exec($instruccion, $output);
+		$output = shell_exec($instruccion." > /dev/null 2>/dev/null &");
 		echo '<pre>';
 		print_r($output);
 		echo '</pre>';
