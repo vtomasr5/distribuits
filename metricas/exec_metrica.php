bc1b5172286cacc46d10ac66445846f14874bdf5
Miquel <mike@Saturno>
2013-05-07 22:40:06 +0200

    Modificado métricas para almacenarlas en ficheros diferentes

--- metricas/exec_metrica.php
+++ metricas/exec_metrica.php
@@ -3,6 +3,7 @@
 *
 *	Script PHP que ejecutara el script python con una llamada desde el navegador
 *	exec_metrica.php?pw=CLAVE&nom=SUFIJO_METRICA
+*	exec_metrica.php?pw=CLAVE
 *	exec_metrica.php?pw=CLAVE&stop=1
 */
 define("CLAVE" , "clave_ejecucion");
@@ -15,11 +16,13 @@ if (isset($_GET['pw'])) {
 	if ($_GET['pw'] == CLAVE) {
 		$instruccion = "cd ".RUTA." && "; //Ejecutamos el comando desde el directorio
 		if (!isset($_GET['stop'])) {
+			$nombre = '';
 			if (isset($_GET['nom'])) {
-				$instruccion .= "/usr/bin/python ".RUTA.SCRIPT." ".$_GET['nom'];
+				$nombre = $_GET['nom'];
 			} else {
-				$instruccion .= "echo a";
+				$nombre = 'original'
 			}
+			$instruccion .= "/usr/bin/python ".RUTA.SCRIPT." ".$nombre;
 		} else { //Paramos la ejecución de los monitores
 			$instruccion = "echo '' > ".RUTA.'exec.stop';			
 		}
